6f5d0a9ecc35fe50c571ada0ee39777005c743d4
root-344 <michimasa8@gmail.com>
2021-02-23 18:05:09 +0900

    フォームオブジェクト機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if @item.user_id != current_user.id
+    if @item.user_id != current_user.id || @item.order.present?
       redirect_to action: :index
     end
   end
