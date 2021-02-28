aed80b8561149809da4aa4aecccab45b19b355ab
root-344 <michimasa8@gmail.com>
2021-03-01 03:22:28 +0900

    エラーメッセージ日本語変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,14 +5,14 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "にはハイフンを入れてください。"}
     validates :city
     validates :address
     validates :building
     validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ }
     validates :token
   end  
-  validates :region_id, numericality: {other_than: 1, message: "Select"}
+  validates :region_id, numericality: {other_than: 1, message: "を選んで下さい"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
