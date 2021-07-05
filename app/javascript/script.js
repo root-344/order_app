428d8f49bb54ee9bf025b8ed3a47ac21823087d1
root-344 <michimasa8@gmail.com>
2021-07-06 03:19:23 +0900

    コメント機能導入（仮）

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,34 @@
 $(function() {
- 
+  function buildHTML(comment){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ：
+                  </strong>
+                  ${comment.text}
+                </p>`
+    return html;
+  };
+ $('#new-comment').submit(function() {
+   e.preventDefault();
+   var formData = new FormData(this);
+   var url = $(this).after('action');
+   $.ajax({
+     url: url,
+     type: "POST",
+     data: formData,
+     dataType: 'json',
+     processData: false,
+     contentType: false
+   });
+   done(function(data){
+    var html = buildHTML(data);
+    $('.comment-box').append(html);
+    $('.comment-text').val('');
+    $('.comment-btn').prop('disabled', false);
+  });
+  fail(function(){
+    alert('error');
+  });
+ });
 });
